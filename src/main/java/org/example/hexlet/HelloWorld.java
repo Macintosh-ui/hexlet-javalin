f870e7a02b3dd4399cfae9c10df83eca7b9bd9e7
Pavlov Denis <pda33@icloud.com>
2024-07-01 20:34:16 +0300

    now its works and starts an local server

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,10 +4,15 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app = new Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
+        var app = Javalin.create(config -> {
+            config.http.generateEtags = true;
+            config.http.prefer405over404 = true;
+            config.http.maxRequestSize = 4000000;
+            config.http.defaultContentType = "stringValue";
+            config.http.asyncTimeout = 0;
+            config.http.disableCompression();
         });
         app.get("/", ctx -> ctx.result("Hello World"));
-        app.start;
+        app.start();
     }
 }
